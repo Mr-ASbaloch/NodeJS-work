24a83f32b3077597004f7256b9a9e7b209cd23d5
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-10-10 02:20:23 -0700

    commit

---
+++ folder-3/text/index.py
@@ -0,0 +1,1 @@
+that is styling file
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello');
+
+// const  math = require('./module.js')
+// console.log( 'value is', math.add(5,6),math.sub(4,19));
+// console.log('hello');
+
+
+
+const { error } = require('console')
+const fs=require ('fs')
+
+// fs.writeFileSync('./word.html','hello my name is saeed')
+
+// fs.cpSync('./word.txt', 'copy.txt')
+// fs.mkdirSync('folder-3/hello.html' , error)
+fs.writeFileSync('./folder-3/text/index.py', 'that is styling file')
+
+// console.log(fs);
