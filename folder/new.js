87b3638d018561db552175e19fb0e30cdd679361
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-10-10 20:31:38 -0700

    commit

---
+++ folder/new.js
@@ -0,0 +1,7 @@
+// // const fs= require('fs')
+// //  const result = fs.readFileSync('word.html', 'utf-8')
+// //  console.log(result);
+ 
+
+// const os= require ('os')
+// console.log(os.cpus().length);
--- index.js
+++ index.js
@@ -5,13 +5,40 @@
 
 
 
-const { error } = require('console')
-const fs=require ('fs')
+// const { error } = require('console')
+// const fs=require ('fs')
 
 // fs.writeFileSync('./word.html','hello my name is saeed')
 
 // fs.cpSync('./word.txt', 'copy.txt')
 // fs.mkdirSync('folder-3/hello.html' , error)
-fs.writeFileSync('./folder-3/text/index.py', 'that is styling file')
+// fs.writeFileSync('./folder-3/text/index.py', 'that is styling file')
+// fs.unlinkSync('./folder-3/text/index.py')
+// fs.readFileSync('copy.txt')
+// fs.appendFileSync('copy.txt' , 'this is updated')
+// fs.renameSync('copy.txt', 'rename.html')
 
 // console.log(fs);
+
+
+
+
+// express work 
+const express = require("express")
+
+const port= 4240
+
+const app =express()
+
+app.get("/",(req, res)=>{
+    res.end("this is home page")
+})
+app.get("/about",(req, res)=>{
+    res.end("this is about page")
+})
+app.get("/login",(req, res)=>{
+    res.end("this is login page")
+})
+app.listen(port, ()=>{
+    console.log('welcome to server of express');
+} )
